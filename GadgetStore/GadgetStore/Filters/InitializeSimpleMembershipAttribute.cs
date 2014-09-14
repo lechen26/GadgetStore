7b4c0b6a25d176746985cb5c849f08d8bcced4b5
lechen26 <levichen24@gmail.com>
2014-09-13 19:56:16 -0700

    Update accountModel

--- GadgetStore/GadgetStore/Filters/InitializeSimpleMembershipAttribute.cs
+++ GadgetStore/GadgetStore/Filters/InitializeSimpleMembershipAttribute.cs
@@ -38,7 +38,7 @@ namespace GadgetStore.Filters
                         }
                     }
 
-                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    WebSecurity.InitializeDatabaseConnection("GadgetEntities", "UserProfile", "UserId", "UserName", autoCreateTables: true);
                 }
                 catch (Exception ex)
                 {
